b8024aeecdc0b7fddaccee29cdaec8b3c30672f3
juan <estresmo@gmail.com>
2023-07-02 22:25:18 -0400

    todo

---
+++ galeria.sql
@@ -0,0 +1,8 @@
+CREATE TABLE galeria (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    artista_id int ,
+    foto1 varchar(300),
+    foto2 varchar(300),
+    foto3 varchar(300),
+    foto4 varchar(300)
+)
